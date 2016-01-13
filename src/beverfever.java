727f81d32946de186254e9e0dc3ae8a184626c0e
Mrfuzzy <league@Mac-mini-2.local>
2016-01-13 15:01:00 -0800

    baby nooo
    speak method…

--- src/beverfever.java
+++ src/beverfever.java
@@ -1,23 +1,28 @@
 
 public class beverfever {
 	public static void main(String[] args) {
-	
-for (int i = 0; i < 3; i++) {
-	
-	 
-	
-		
-	
-	for (int j = 0; j < 3; j++) {
-		System.out.println("baby");
-		
-	}
-	if (i==1) {
-		System.out.println("no");
-		}else {
-	System.out.println("oh");
+
+		for (int i = 0; i < 3; i++) {
+
+			for (int j = 0; j < 3; j++) {
+				speak("baby");
+
+			}
+			if (i == 1) {
+				speak("no");
+			} else {
+				speak("oh");
+			}
+
 		}
-	
-}
-}
+	}
+
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
