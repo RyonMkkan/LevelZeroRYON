c4b4b863420481c21651618ec64e40731de77040
Ryon <league@WorthyMini.local>
2017-03-09 16:56:54 -0800

    blast to the past
    marty!

--- src/PentagonCrazy.java
+++ src/PentagonCrazy.java
@@ -14,11 +14,11 @@ crazy.penDown();
 		// 8. Make the robot go at maximum speed (10)
 crazy.setSpeed(10);
 		// 9. Set the pen to a color that you like for the shape
-
+crazy.setRandomPenColor();
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-int numberofsides=5;
+int numberofsides=4;
 		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-int angleofturn=(360/5);
+int angleofturn=(360/4);
 		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
 for (int i = 0; i < 200; i++) {
 	
@@ -28,8 +28,9 @@ for (int i = 0; i < 200; i++) {
 			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
 	
 			// 6. Turn the robot the amount in your angle variable
-	crazy.turn(360/5);
+	crazy.turn(360/4);
 			// 11. Turn the robot one more degree
+	crazy.turn(1);
 }
 	}
 
